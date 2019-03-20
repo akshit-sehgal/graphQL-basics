03cf2a0bad0913469fe4cce5d01ac525cde20990
Akshit Sehgal <akshitsehgal95@gmail.com>
2019-03-20 18:33:04 +0530

    Working with arrays (for scalar types)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { GraphQLServer } from 'graphql-yoga';
 const typeDefs = `
     type Query {
         greeting(name: String, position: String): String!
-        add(a: Float!, b: Float!): Float!
+        add(numbers: [Float!]!): Float!
+        grades:[Int!]!
         me: User!
         post: Post!
     }
@@ -30,7 +31,15 @@ const resolvers = {
             return `Welcome to the playground ${args.name && args.position ? `${args.name} (${args.position})` : ''}!`
         },
         add(parent, args, context, info) {
-            return args.a + args.b;
+            if (args.numbers.length === 0) {
+                return 0;
+            }
+            return args.numbers.reduce((accumulator, currentValue) => {
+                return accumulator += currentValue;
+            });
+        },
+        grades(parent, args, context, info) {
+            return [98, 85, 70, 90];
         },
         me() {
             return {
