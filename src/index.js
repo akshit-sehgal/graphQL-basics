789db9f24a045f99e1934c1e6edf4cd6b8decd2f
Akshit Sehgal <akshitsehgal95@gmail.com>
2019-03-21 10:24:45 +0530

    Using Input Type

--- src/index.js
+++ src/index.js
@@ -76,9 +76,25 @@ const typeDefs = `
         post: Post!
     }
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!):Post!
-        createComment(text: String!, author: ID!, post: ID!): Comment!
+        createUser(data: CreateUserInput!): User!
+        createPost(data: CreatePostInput!):Post!
+        createComment(data: CreateCommentInput!): Comment!
+    }
+    input CreateUserInput{
+        name: String!
+        email: String!
+        age: Int
+    }
+    input CreatePostInput{
+        title: String! 
+        body: String! 
+        published: Boolean! 
+        author: ID!
+    }
+    input CreateCommentInput{
+        text: String!
+        author: ID!
+        post: ID!
     }
     type User{
         id: ID!
@@ -138,37 +154,32 @@ const resolvers = {
     },
     Mutation: {
         createUser(parent, args, context, info) {
-            const emailTaken = users.some((user) => user.email === args.email);
+            const emailTaken = users.some((user) => user.email === args.data.email);
             if (emailTaken) {
                 throw new Error('E-Mail ID already in use.')
             }
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age ? args.age : null
+                ...args.data
             }
             users.push(user);
             return user;
         },
         createPost(parent, args, context, info) {
-            const userExists = users.some((user) => user.id === args.author)
+            const userExists = users.some((user) => user.id === args.data.author)
             if (!userExists) {
                 throw new Error('Author does not exist');
             }
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args.data
             }
             posts.push(post);
             return post;
         },
         createComment(parent, args, context, info) {
-            const userExists = users.some((user) => user.id === args.author);
-            const postExists = posts.some((post) => post.id === args.post && post.published)
+            const userExists = users.some((user) => user.id === args.data.author);
+            const postExists = posts.some((post) => post.id === args.data.post && post.published)
             if (!userExists) {
                 throw new Error('Author does not exist')
             }
@@ -177,9 +188,7 @@ const resolvers = {
             }
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args.data
             }
             comments.push(comment);
             return comment;
